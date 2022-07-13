1bae1f62cf7c61e14f373644e2cd0cce50c82f27
Alona_Tyshchenkogit config --global user.email alona.tyshchenko.dev.gmail.com <alona.tyshchenko.dev@gmail.com>
2022-07-13 17:33:42 +0300

    add service and quard

--- src/app/modules/login-page/helpers/consants.ts
+++ src/app/modules/login-page/helpers/consants.ts
@@ -1,16 +1,2 @@
-import { HTTP_INTERCEPTORS, HttpHeaders } from '@angular/common/http';
-import { AuthInterceptor } from './auth.interceptor';
+export const ApiUrl: string = 'http://localhost:3000/auth/login';
 
-export const AuthApi: string = 'http://localhost:3000/users';
-export const TOKEN_NAME: string = 'JWT token';
-export const httpOptions = {
-  headers: new HttpHeaders({'Content-Type': 'application/json'})
-};
-export const TOKEN_KEY = 'auth-token';
-export const TOKEN_HEADER_KEY = 'Authorization';
-export const authInterceptorProviders = [
-  {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
-];
-
-
-export const FAKE_JWT_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6InRlc3QgZm9ybSBCdWlsZCIsImlhdCI6MTcyNzM3OTc1M30.odCkr3Xe9LXXjNNCu_IDhtNpbm4876Cbb61cOEjiP4Q';
